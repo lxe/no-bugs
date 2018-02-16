f12d5335d0398c44470a702429d91b0661eefbd9
Eugene G <huncode@users.noreply.github.com>
2018-02-16 16:04:59 +0100

    Meditate on bugs free development
    Happy debugging!

--- no-bugs.js
+++ no-bugs.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
 console.log('No bugs here!');
+
+const isNode = typeof process !== 'undefined';
+
+if (!isNode) {
+  window.addEventListener('error', (e) => {
+    e.preventDefault();
+  });
+} else {
+  process.on('uncaughtException', () => {});
+}
